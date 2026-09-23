c0bd40b57b545d5276ca4086a464b049a7af6f79
Dominik Russenberger <dominik.russenberger@students.fhnw.ch>
2026-09-23 11:56:17 +0200

    week 1

--- Makefile
+++ Makefile
@@ -1,6 +1,16 @@
-CFLAGS := -Os -s
-export CFLAGS
+#CC := gcc
+# -mfpmath=387
+SMALL := -Wl,-z,norelro -Wl,-z,execstack -Wl,-z,noseparate-code -Wl,--gc-sections -Wl,--build-id=none -fno-asynchronous-unwind-tables -fno-stack-clash-protection -fno-stack-protector -fcf-protection=none -no-pie -fno-pie -fno-plt -fwhole-program -Wl,--no-eh-frame-hdr
+CFLAGS := -Ofast -g -Wall -Wextra -Wpedantic -std=c99 $(SMALL)
+#CFLAGS := -Ofast -g -Wall -Wextra
+#CFLAGS := -g -Wall
 
 default:
-	$(CC) $(CFLAGS) -o test test.c `sdl2-config --cflags --libs`
+	$(CC) $(CFLAGS) -fverbose-asm -S test.c `sdl2-config --cflags --libs` -lm
+	$(CC) $(CFLAGS) -o test test.c `sdl2-config --cflags --libs` -lm
+
+small: default
 	@ls -l test
+	@cp test test2
+	@sstrip test2
+	@lz4pack test2
--- test.c
+++ test.c
@@ -1,14 +1,152 @@
+/* 
+References:
+Coding style https://www.kernel.org/doc/html/latest/process/coding-style.html
+SDL2 installation/basic compile https://wiki.libsdl.org/SDL2/Installation
+SDL2 pixel drawing https://gigi.nullneuron.net/gigilabs/sdl2-pixel-drawing/
+SDL2 pixel drawing https://gist.github.com/mmozeiko/729860eeb414f1a2ee345d9d3ab4dd4e
+https://web.archive.org/web/20160326085538/http://content.gpwiki.org/index.php/SDL:Tutorials:Drawing_and_Filling_Circles
+https://web.archive.org/web/20120420111824/http://www.libsdl.org/intro.en/usingvideo.html
+SDL3 surfaces and colors https://www.studyplan.dev/sdl3/sdl3-surfaces-colors#sdl3-surfaces-and-colors
+
+pixel formats: https://wiki.libsdl.org/SDL2/CategoryPixels
+save "screenshot": https://wiki.libsdl.org/SDL3/SDL_SaveBMP
+
+going offtopic:
+https://en.wikipedia.org/wiki/VESA_BIOS_Extensions#VBE_mode_numbers
+secret modes, esp. what TomCatAbaddon posted https://www.pouet.net/topic.php?which=11672&page=1
+
+Coordinate system: right-handed, Z up
+
+AI: Google search for "SDL_MUSTLOCK"
+
+*/
+
+#define GAMMA 2.2
+#define FLOAT float
+#define DEBUG 0
+#define YOLO  1		// activate if you don't care about handling edge cases
+
 #include <SDL.h>
- 
-int main(int argc, char ** argv)
+#include <math.h>
+#include <assert.h>
+#include <stdbool.h>
+
+#if DEBUG
+#define dprintf(...) printf(__VA_ARGS__)
+#else
+#define dprintf(...)
+#define NDEBUG 1
+#endif
+
+typedef struct vec3 {
+	FLOAT x;
+	FLOAT y;
+	FLOAT z;
+} vec3;
+
+uint8_t map_component(FLOAT c)
 {
-    SDL_Init(SDL_INIT_VIDEO);
+	if (c > 1)
+		c = 1;
+	if (c < 0)
+		c = 0;
+
+	return 255*pow(c, 1/GAMMA);
+}
+
+uint32_t colormap(vec3 color)
+{
+	uint8_t r = map_component(color.x);
+	uint8_t g = map_component(color.y);
+	uint8_t b = map_component(color.z);
+
+	//dprintf("lin %u %u %u\n", r, g, b);
+	return (r << 16) + (g << 8) + (b << 0); // bgra32
+	// return (r << 0) + (g << 8) + (b << 16); // rgba32
+}
+
+vec3 add(vec3 a, vec3 b)
+{
+	a.x = a.x + b.x;
+	a.y = a.y + b.y;
+	a.z = a.z + b.z;
+	return a;
+}
+
+vec3 scalar_mult(vec3 a, FLOAT amount)
+{
+	a.x = a.x * amount; 
+	a.y = a.y * amount; 
+	a.z = a.z * amount; 
+	return a;
+}
+
+vec3 lerp(vec3 a, vec3 b, FLOAT amount)
+{
+	assert(amount >= 0);
+	assert(amount <= 1);
+	return add(scalar_mult(a, amount), scalar_mult(b, 1-amount));
+}
+
+void set_pixel(SDL_Surface *surface, int x, int y, uint32_t pixel)
+{
+	// XXX constant "4"
+	uint8_t *target_pixel = (uint8_t*)surface->pixels + (y * surface->pitch) + (x * 4);
+        *(uint32_t*)target_pixel = pixel;
+}
+
+// treat "surface" as an opaque data structure
+void render(void* surface, int w, int h)
+{
+	vec3 red  = { 1, 0, 0 };
+	vec3 green  = { 0, 1, 0 };
+	
+	for(int j=0; j < h; j++){
+		for(int i=0; i < w; i++){
+			FLOAT amount = (FLOAT)i/(FLOAT)w;
+			//vec3 color = lerp(red, green, amount);
+			vec3 color = lerp(green, red, amount);
+			dprintf("amount %f\n", amount);
+			uint32_t pixel = colormap(color);
+			set_pixel(surface, i, j, pixel);
+		}
+	}
+}
  
-    SDL_Window * screen = SDL_CreateWindow("My SDL Empty Window",
-        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
-    SDL_Delay(3000);
- 
-    SDL_Quit();
- 
-    return 0;
+void wait()
+{
+	SDL_Event event;
+	bool quit = false;
+	while (!quit) {
+		SDL_WaitEvent(&event);
+
+		switch (event.type) {
+		case SDL_QUIT:
+			quit = true;
+			break;
+		}
+	}
+}
+
+
+int main()
+{
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Window* window = SDL_CreateWindow("computer graphics dominikr",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+	SDL_Surface* surface = SDL_GetWindowSurface(window);
+	if (SDL_MUSTLOCK(surface) && !YOLO) {
+		SDL_LockSurface(surface);
+	}
+
+	render(surface, surface->w, surface->h);
+
+	if (SDL_MUSTLOCK(surface) && !YOLO) {
+		SDL_UnlockSurface(surface);
+	}
+	SDL_UpdateWindowSurface(window);
+
+	wait();
+
+	return 0;
 }
