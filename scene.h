4b814425ae9d65ccc7368b8c9279759d6561e7d3
Dominik Russenberger <dominik.russenberger@students.fhnw.ch>
2026-09-25 22:12:56 +0200

    formatting

--- scene.h
+++ scene.h
@@ -1,14 +1,14 @@
-#define RED	{ 0.8, 0,  0 }
-#define GREEN	{ 0, 0.8,  0 }
-#define BLUE	{ 0, 0, 0.8 }
+#define RED	{ 0.8,   0,   0 }
+#define GREEN	{   0, 0.8,   0 }
+#define BLUE	{   0,   0, 0.8 }
 #define CYAN	{ 0.5, 0.8, 0.8 }
 #define GRAY	{ 0.6, 0.6, 0.6 }
-#define WHITE	{ 1, 1,  1 }
-#define YELLOW	{ 0.8, 0.8,  0 }
+#define WHITE	{   1,   1,   1 }
+#define YELLOW	{ 0.8, 0.8,   0 }
 
-#define EYE	{ 0, 0, -4 }
-#define LOOK	{ 0, 0,  6 }
-#define UP	{ 0, 1, 0 }
+#define EYE	{   0,   0,  -4 }
+#define LOOK	{   0,   0,   6 }
+#define UP	{   0,   1,   0 }
 #define FOV	36
 
 sphere scene[] = {
